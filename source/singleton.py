8b626a9d3dd1ebf5088b31a005cf54a62eccaf95
dseraogi <dseraogi@its.jnj.com>
2019-09-08 00:11:40 +0530

    Created a template Singleton class for Connection classes

---
+++ source/singleton.py
@@ -0,0 +1,39 @@
+class SingletonMetaClass(type):
+    """
+    This is Metaclass to befine the behavior of a sungleton class
+    """
+    _instances_dict = {}
+
+    def __call__(cls, *args, **kwargs):
+        print(cls._instances_dict)
+        if not cls._instances_dict.get(cls):
+            cls._instances_dict[cls] = super(SingletonMetaClass, cls).\
+                __call__(*args,**kwargs)
+        return cls._instances_dict[cls]
+
+
+class ConnectionClass1(metaclass=SingletonMetaClass):
+    """
+    This is a dummy connection class
+    """
+    def __init__(self):
+        print(f"Initiating a class of type {type(self)}")
+        self._connection_string = "ConnectionClass1"
+
+    @property
+    def get_connection_string(self):
+        return self._connection_string
+
+
+class ConnectionClass2(metaclass=SingletonMetaClass):
+    """
+    This is a dummy connection class
+    """
+    def __init__(self):
+        print(f"Initiating a class of type {type(self)}")
+        self._connection_string = "ConnectionClass2"
+
+
+if __name__ == "__main__":
+    print(ConnectionClass1() is ConnectionClass1())
+    print(ConnectionClass2() is ConnectionClass2())
